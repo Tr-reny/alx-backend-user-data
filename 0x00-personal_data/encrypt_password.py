e13fb278c40c4f2339e36fb5e561854a70d9c7dd
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-06-08 23:34:12 +0300

    updates

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""" Encrypt passwords"""
-
+"""A module for encrypting passwords.
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """returns a salted, hashed password, which is a byte string."""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    """Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """check if password is valid"""
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    """Checks is a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
